6f396708ec943c1eeff78edd9011061fe5e07cbe
SergioFdezRc <s.fernandez.rincon@gmail.com>
2020-12-05 12:08:24 +0100

    Se ha añadido jsonify en todas las respuestas

--- swagger_server/controllers/account_controller.py
+++ swagger_server/controllers/account_controller.py
@@ -21,13 +21,13 @@ def add_account(account):  # noqa: E501
     db = PostgresDB()
     info = db.get_account_id_by_username(account.username)
     if "Error" in info:
-        return info
+        return jsonify(msg=info)
     if len(info) > 0:
-        return 'The user %s already exists' % account.username, 409
+        return jsonify(msg='The user %s already exists' % account.username), 409
     error = db.add_new_account(account.username, account.password, account.birthdate, account.age)
     if error:
-        return error
-    return "OK. User %s created" % account.username, 200
+        return jsonify(msg=error)
+    return jsonify(msg="OK. User %s created" % account.username), 200
 
 
 def delete_account(account_id):  # noqa: E501
@@ -44,14 +44,14 @@ def delete_account(account_id):  # noqa: E501
 
     account = db.get_account_by_id(account_id)
     if "Error" in account:
-        return account
+        return jsonify(msg=account)
     if len(account) > 0:
         error = db.delete_account(account_id)
         if error:
-            return error
-        return 'The account has been removed', 200
+            return jsonify(msg=error)
+        return jsonify(msg='The account has been removed'), 200
     else:
-        return 'Account not found' % account_id, 404
+        return jsonify(msg='Account not found' % account_id), 404
 
 
 def get_account(account_id):  # noqa: E501
@@ -67,14 +67,14 @@ def get_account(account_id):  # noqa: E501
     db = PostgresDB()
     account = db.get_account_by_id(account_id)
     if "Error" in account:
-        return account
+        return jsonify(msg=account)
     if len(account) > 0:
         account = account[0]
         return jsonify({"username": account[1],
                         "birthdate": account[3],
                         "age": account[4]})
 
-    return 'Account not found', 404
+    return jsonify(msg='Account not found'), 404
 
 
 def update_account(update_account):  # noqa: E501
@@ -97,9 +97,9 @@ def update_account(update_account):  # noqa: E501
     if len(info) > 0:
         info = info[0]
         if update_account.old_password == update_account.new_password:
-            return 'Passwords match', 409
+            return jsonify(msg='Passwords match'), 409
         error = db.update_account(info[0], update_account.new_password)
         if error:
-            return error
-        return "Password changed", 200
-    return 'Incorrect password', 204
+            return jsonify(msg=error)
+        return jsonify(msg="Password changed"), 200
+    return jsonify(msg='Incorrect password'), 204
